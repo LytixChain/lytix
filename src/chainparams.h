b1d8532ce12e7e9d3e7bf895fc3277b07544652e
Nat Holloway <32520545+faetos@users.noreply.github.com>
2018-11-08 13:38:04 -0600

    Masternode change to 5000

--- src/chainparams.h
+++ src/chainparams.h
@@ -17,7 +17,7 @@
 #include "libzerocoin/Params.h"
 #include <vector>
 
-#define MASTERNODE_COLLATERAL_AMOUNT 500
+#define MASTERNODE_COLLATERAL_AMOUNT 5000
 
 typedef unsigned char MessageStartChars[MESSAGE_START_SIZE];
 
