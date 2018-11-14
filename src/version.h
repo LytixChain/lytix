7ed6218a98b915e15ac4dc61908affc1353d4808
faetos <faetos@yahoo.com>
2018-11-14 11:48:30 -0600

    work to fix stuck block 14944

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 71029;
+static const int PROTOCOL_VERSION = 71030;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -20,11 +20,11 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 71028;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 71029;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 71029;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 71030;
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
-static const int MIN_PEER_MNANNOUNCE = 71029;
+static const int MIN_PEER_MNANNOUNCE = 71030;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
