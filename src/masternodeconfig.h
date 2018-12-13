2dabd8048f9311a4ee82cebf44212adb88f7f7f8
faetos <faetos@yahoo.com>
2018-12-12 22:54:25 -0600

    missed a senmi-colon

--- src/masternodeconfig.h
+++ src/masternodeconfig.h
@@ -99,7 +99,7 @@ public:
     bool read(std::string& strErr);
     void writeToMasternodeConf();
     void add(std::string alias, std::string ip, std::string privKey, std::string txHash, std::string outputIndex);
-    void deleteAlias(int count)
+    void deleteAlias(int count);
 
     std::vector<CMasternodeEntry>& getEntries()
     {
