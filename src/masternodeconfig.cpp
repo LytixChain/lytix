ba100f5c10e86ae7c9c3186d4d5dfd0feebecf9b
faetos <faetos@yahoo.com>
2018-12-14 23:37:53 -0600

    backing out masternodegui changes
    Signed-off-by: faetos <faetos@yahoo.com>

--- src/masternodeconfig.cpp
+++ src/masternodeconfig.cpp
@@ -3,13 +3,11 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-// clang-format off
-#include "net.h"
+#include "netbase.h"
 #include "masternodeconfig.h"
 #include "util.h"
 #include "ui_interface.h"
-#include "base58.h"
-// clang-format on
+#include <base58.h>
 
 CMasternodeConfig masternodeConfig;
 
@@ -60,15 +58,25 @@ bool CMasternodeConfig::read(std::string& strErr)
             }
         }
 
+	int port = 0;
+        std::string hostname = "";
+        SplitHostPort(ip, port, hostname);
+        if(port == 0 || hostname == "") {
+            strErr = _("Failed to parse host:port string") + "\n"+
+                     strprintf(_("Line: %d"), linenumber) + "\n\"" + line + "\"";
+            streamConfig.close();
+            return false;
+        }
+
         if (Params().NetworkID() == CBaseChainParams::MAIN) {
-            if (CService(ip).GetPort() != 27071) {
+            if (port != 27071) {
                 strErr = _("Invalid port detected in masternode.conf") + "\n" +
                          strprintf(_("Line: %d"), linenumber) + "\n\"" + line + "\"" + "\n" +
                          _("(must be 27071 for mainnet)");
                 streamConfig.close();
                 return false;
             }
-        } else if (CService(ip).GetPort() == 27071) {
+        } else if (port == 27071) {
             strErr = _("Invalid port detected in masternode.conf") + "\n" +
                      strprintf(_("Line: %d"), linenumber) + "\n\"" + line + "\"" + "\n" +
                      _("(27071 could be used only on mainnet)");
@@ -94,44 +102,4 @@ bool CMasternodeConfig::CMasternodeEntry::castOutputIndex(int &n)
     }
 
     return true;
-}
-
-void CMasternodeConfig::clear()
-{
-	entries.clear();
-}
-
-void CMasternodeConfig::deleteAlias(int count)
-{
-	count = count - 1;
-	entries.erase(entries.begin()+count);
-}
-
-void CMasternodeConfig::writeToMasternodeConf()
-{
-    boost::filesystem::path pathMasternodeConfigFile = GetMasternodeConfigFile();
-    boost::filesystem::ifstream streamConfig(pathMasternodeConfigFile);
-
-    FILE* configFile = fopen(pathMasternodeConfigFile.string().c_str(), "w");
-
-	// Add file header back as each time this runs it restarts the file
-    std::string strHeader = "# Masternode config file\n"
-                            "# Format: alias IP:port masternodeprivkey collateral_output_txid collateral_output_index\n"
-                            "# Example: mn1 127.0.0.2:27071 93HaYBVUCYjEMeeH1Y4sBGLALQZE1Yc1K64xiqgX37tGBDQL8Xg 2bcd3c84c84f87eaa86e4e56834c92927a07f9e18718810b92e0d0324456a67c 0\n";
-    fwrite(strHeader.c_str(), std::strlen(strHeader.c_str()), 1, configFile);
-	
-	std::string masternodeAliasBase = "";
-	
-	BOOST_FOREACH (CMasternodeConfig::CMasternodeEntry mne, masternodeConfig.getEntries()) {
-		// Orders configs in proper strings
-		std::string masternodeAliasLine  = mne.getAlias() + " " + mne.getIp() + " " + mne.getPrivKey() + " " + mne.getTxHash() + " " + mne.getOutputIndex() + "\n";
-		masternodeAliasBase = masternodeAliasBase + masternodeAliasLine;
-	}
-	//Writes it to the string
-	fwrite(masternodeAliasBase.c_str(), std::strlen(masternodeAliasBase.c_str()), 1, configFile);
-	// When done adding all the masternodes to the config close the file	
-    fclose(configFile);
-	clear();
-	std::string strErr;
-	read(strErr);
 }
--- src/qt/masternodelist.h
+++ src/qt/masternodelist.h
@@ -10,15 +10,10 @@
 #include "platformstyle.h"
 #include "sync.h"
 #include "util.h"
-#include "wallet.h"
 
 #include <QMenu>
 #include <QTimer>
 #include <QWidget>
-
-#include "walletmodel.h"
-#include <QDialog>
-#include <QString>
 
 #define MY_MASTERNODELIST_UPDATE_SECONDS 60
 #define MASTERNODELIST_UPDATE_SECONDS 15
@@ -49,7 +44,6 @@ public:
     void setWalletModel(WalletModel* walletModel);
     void StartAlias(std::string strAlias);
     void StartAll(std::string strCommand = "start-all");
-    void deleteAlias(std::string Alias);
 
 private:
     QMenu* contextMenu;
@@ -72,16 +66,9 @@ private:
 
 private Q_SLOTS:
     void showContextMenu(const QPoint&);
-    void deleteAlias();
-    void copyAlias();
     void on_startButton_clicked();
-    void on_editConfigureMasternode_clicked();
     void on_startAllButton_clicked();
     void on_startMissingButton_clicked();
-    void on_configureMasternodeButton_clicked();
-    void openEditConfigureMasternodePage(QString strAlias, QString strIP, QString strPrivKey, QString strTxHash, QString strOutputIndex, int count);
-    void on_getMNPrivKeyButton_clicked();
-    void on_getOutputsButton_clicked();
     void on_tableWidgetMyMasternodes_itemSelectionChanged();
     void on_UpdateButton_clicked();
 };
