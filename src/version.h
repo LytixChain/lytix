e8676e89bb52ab50fae07d0b0fa4bd4e0592e9ce
faetos <faetos@yahoo.com>
2018-12-24 15:40:02 -0600

    modified version.h for Lytix history
    Signed-off-by: faetos <faetos@yahoo.com>

--- src/version.h
+++ src/version.h
@@ -17,7 +17,7 @@ static const int PROTOCOL_VERSION = 71030;
 static const int INIT_PROTO_VERSION = 209;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 70077;
+static const int GETHEADERS_VERSION = 71027;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 71029;
@@ -28,16 +28,16 @@ static const int MIN_PEER_MNANNOUNCE = 71030;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
-static const int CADDR_TIME_VERSION = 31402;
+static const int CADDR_TIME_VERSION = 71027;
 
 //! BIP 0031, pong message, is enabled for all versions AFTER this one
-static const int BIP0031_VERSION = 60000;
+static const int BIP0031_VERSION = 71027;
 
 //! "mempool" command, enhanced "getdata" behavior starts with this version
-static const int MEMPOOL_GD_VERSION = 600002;
+static const int MEMPOOL_GD_VERSION = 71027;
 
 //! "filter*" commands are disabled without NODE_BLOOM after and including this version
-static const int NO_BLOOM_VERSION = 70005;
+static const int NO_BLOOM_VERSION = 71027;
 
 
 #endif // BITCOIN_VERSION_H
