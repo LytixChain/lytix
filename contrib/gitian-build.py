3d70036a9acfc9977d64c146a3b4f8c16667e5e8
faetos <faetos@yahoo.com>
2019-01-18 20:49:06 -0600

    gitian-builder.py trusty rollback

--- contrib/gitian-build.py
+++ contrib/gitian-build.py
@@ -31,7 +31,7 @@ def setup():
     if not os.path.isdir('bitcoin'):
         subprocess.check_call(['git', 'clone', 'https://github.com/lytixchain/lytix.git'])
     os.chdir('gitian-builder')
-    make_image_prog = ['bin/make-base-vm', '--suite', 'bionic', '--arch', 'amd64']
+    make_image_prog = ['bin/make-base-vm', '--suite', 'trusty', '--arch', 'amd64']
     if args.docker:
         make_image_prog += ['--docker']
     elif not args.kvm:
