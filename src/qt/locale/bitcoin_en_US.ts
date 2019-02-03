e929ca37a87e168ba07c275d383c94a3e5bd32f7
faetos <faetos@yahoo.com>
2019-02-02 18:47:01 -0600

    fixing about copyrights [ci ckip]

--- src/qt/locale/bitcoin_en_US.ts
+++ src/qt/locale/bitcoin_en_US.ts
@@ -4282,8 +4282,12 @@ for example: alertnotify=echo %%s | mail -s "Lytix Alert" admin@foo.com
         <translation>Copyright (C) 2014-%i The Dash Core Developers</translation>
     </message>
     <message>
-        <source>Copyright (C) 2015-%i The Lytix Core Developers</source>
-        <translation>Copyright (C) 2015-%i The Lytix Core Developers</translation>
+        <source>Copyright (C) 2015-%i The PIVX Core Developers</source>
+        <translation>Copyright (C) 2015-%i The PIVX Core Developers</translation>
+    </message>
+    <message>
+        <source>Copyright (C) 2018-%i The Lytix Core Developers</source>
+        <translation>Copyright (C) 2018-%i The Lytix Core Developers</translation>
     </message>
     <message>
         <source>Corrupted block database detected</source>
