7351ad2a34e0bfa87365a9db342964f87a4ad7ee
faetos <faetos@yahoo.com>
2019-02-19 14:51:02 -0600

    rolling back version

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 71032;
+static const int PROTOCOL_VERSION = 71031;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -21,10 +21,10 @@ static const int GETHEADERS_VERSION = 71027;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 71029;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 71032;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 71031;
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
-static const int MIN_PEER_MNANNOUNCE = 71031;
+static const int MIN_PEER_MNANNOUNCE = 71030;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
