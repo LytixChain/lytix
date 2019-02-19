146abcc84641ffcc9364b62005824d7d42c8caef
faetos <faetos@yahoo.com>
2019-02-19 12:49:55 -0600

    changing testnet to 300

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -328,7 +328,7 @@ public:
         nMinerThreads = 1;
         nTargetTimespan = 24 * 60 * 60; // Lytix: 1 day
         nTargetSpacing = 60;  // Lytix: 1 minute
-        nLastPOWBlock = 500;
+        nLastPOWBlock = 300;
         nMaturity = 15;
         nMasternodeCountDrift = 2;
         // nModifierUpdateBlock = 0; //approx Mon, 17 Apr 2017 04:00:00 GMT
--- src/main.cpp
+++ src/main.cpp
@@ -1914,7 +1914,7 @@ int64_t GetBlockValue(int nHeight)
         CoinAmount = static_cast<int64_t>(1 * COIN);
     } else if ( nHeight > 50 && nHeight <= Params().LAST_POW_BLOCK()) {
         CoinAmount = static_cast<int64_t>(30 * COIN);
-    } else if (nHeight > Params().LAST_POW_BLOCK() && nHeight <= 550) {
+    } else if (nHeight > Params().LAST_POW_BLOCK() && nHeight <= 350) {
         CoinAmount = static_cast<int64_t>(100 * COIN);
     } else if (nHeight > 550 && nHeight <= (1 * DropTime)) {
         CoinAmount = static_cast<int64_t>(30 * COIN);
