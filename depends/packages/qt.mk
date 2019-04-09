48bc9109d48fbc08a5934a3b9e7a80de21164f55
faetos <faetos@yahoo.com>
2019-04-09 00:12:40 -0500

    [ci skip] linux qt.mk change

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -96,7 +96,6 @@ endif
 $(package)_config_opts_linux += -system-freetype
 $(package)_config_opts_linux += -no-feature-sessionmanager
 $(package)_config_opts_linux += -fontconfig
-$(package)_config_opts_linux += -qt-xcbcommon-x11
 $(package)_config_opts_linux += -qt-xcb
 $(package)_config_opts_linux += -no-opengl
 $(package)_config_opts_arm_linux += -platform linux-g++ -xplatform lytix-linux-g++
