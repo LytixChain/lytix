128fa6576e8a1290ec9ea7e51c261c503041ffd8
faetos <faetos@yahoo.com>
2019-05-15 09:31:00 -0500

    debug test message in activemaxnode.cpp [ci skip]

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2014-2016 The Dash developers
 // Copyright (c) 2015-2018 The PIVX developers
+// Copyright (c) 2018-2019 The Lytix developer
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -12,7 +13,7 @@
 #include "spork.h"
 
 //
-// Bootup the Masternode, look for a 1000 LYTX input and register on the network
+// Bootup the Masternode, look for a 5000 LYTX input and register on the network
 //
 void CActiveMasternode::ManageStatus()
 {
