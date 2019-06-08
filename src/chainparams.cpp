af71b1519a754047d9683ebf99d5606afb6354c5
faetos <faetos@yahoo.com>
2019-06-08 11:56:30 -0500

    adding checkpoint for block 252,000

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -65,11 +65,12 @@ static Checkpoints::MapCheckpoints mapCheckpoints =
     (30000, uint256("0x00000000c568c33cf4b881a256e779e7c73b7e326db2f603a0ce16277506baa5"))
     (60000, uint256("0x000000000222d4eb292e05aa35b4959f0705c77520d5e3c4980172988483db36"))
     (101000, uint256("0xdf8bbe946d173b8bb266ddec0195dc415baadd332dfbedd79b4668ba69893aac"))
-    (170000, uint256("0xdfb4f14e31f62318ada32144417258b7ce040865343477641aabb0ff739508f1"));
+    (170000, uint256("0xdfb4f14e31f62318ada32144417258b7ce040865343477641aabb0ff739508f1"))
+    (252000, uint256("0xb9e9141619cd48dddbd75a033f171b3cd9317f748d6a1ba8ce455ad2662c28d2"));
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
-    1554992062, // * UNIX timestamp of last checkpoint block
-    272965,    // * total number of transactions between genesis and last checkpoint
+    1559975109, // * UNIX timestamp of last checkpoint block
+    447416,    // * total number of transactions between genesis and last checkpoint
                 //   (the tx=... number in the SetBestChain debug.log lines)
     1440        // * estimated number of transactions per day after checkpoint
 };
