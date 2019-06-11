976315bfb19e0c108f4aa6a73ab883220dda3ca3
faetos <faetos@yahoo.com>
2019-06-10 19:34:30 -0500

    changing testnet last pow

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -333,7 +333,7 @@ public:
         nMinerThreads = 1;
         nTargetTimespan = 24 * 60 * 60; // Lytix: 1 day
         nTargetSpacing = 120;  // Lytix: 2 minute
-        nLastPOWBlock = 2500;
+        nLastPOWBlock = 1200;
         nMaturity = 15;
         nMasternodeCountDrift = 2;
         nMaxnodeCountDrift = 2;
