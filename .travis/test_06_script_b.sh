92f83b5aeb34aef3bbaad1f2eb9cce5685280606
faetos <faetos@yahoo.com>
2019-07-10 23:26:38 -0500

    trying new travis

--- .travis/test_06_script_b.sh
+++ .travis/test_06_script_b.sh
@@ -6,7 +6,7 @@
 
 export LC_ALL=C.UTF-8
 
-cd "build/lytix-$HOST" || (echo "could not enter distdir build/lytix-$HOST"; exit 1)
+cd "build/pivx-$HOST" || (echo "could not enter distdir build/pivx-$HOST"; exit 1)
 
 if [ "$RUN_UNIT_TESTS" = "true" ]; then
   BEGIN_FOLD unit-tests
@@ -16,12 +16,6 @@ fi
 
 if [ "$RUN_FUNCTIONAL_TESTS" = "true" ]; then
   BEGIN_FOLD functional-tests
-  DOCKER_EXEC test/functional/test_runner.py --ci --combinedlogslen=4000 --coverage --quiet --failfast
-  END_FOLD
-fi
-
-if [ "$RUN_FUZZ_TESTS" = "true" ]; then
-  BEGIN_FOLD fuzz-tests
-  DOCKER_EXEC test/fuzz/test_runner.py -l DEBUG ${DIR_FUZZ_IN}
+  DOCKER_EXEC test/functional/test_runner.py --combinedlogslen=4000 ${TEST_RUNNER_EXTRA} --quiet
   END_FOLD
 fi
