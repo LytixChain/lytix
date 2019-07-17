429653426af6e4c8fdfc9f81e525f620f573dfdf
faetos <faetos@yahoo.com>
2019-07-17 10:43:04 -0500

    rollback X libs

--- depends/packages/libXext.mk
+++ depends/packages/libXext.mk
@@ -1,12 +1,16 @@
 package=libXext
-$(package)_version=1.3.4
+$(package)_version=1.3.2
 $(package)_download_path=http://xorg.freedesktop.org/releases/individual/lib/
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=59ad6fcce98deaecc14d39a672cf218ca37aba617c9a0f691cac3bcd28edf82b
+$(package)_sha256_hash=f829075bc646cdc085fa25d98d5885d83b1759ceb355933127c257e8e50432e0
 $(package)_dependencies=xproto xextproto libX11 libXau
 
 define $(package)_set_vars
   $(package)_config_opts=--disable-static
+endef
+
+define $(package)_preprocess_cmds
+  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub .
 endef
 
 define $(package)_config_cmds
