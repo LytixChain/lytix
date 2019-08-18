0bd69389c34650924968ebb8f134473d84d70eb3
faetos <faetos@yahoo.com>
2019-08-17 20:01:26 -0500

    include xcb in qt_bitcoin

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -60,7 +60,9 @@
 #if QT_VERSION < 0x050400
 Q_IMPORT_PLUGIN(AccessibleFactory)
 #endif
-#if defined(QT_QPA_PLATFORM_WINDOWS)
+#if defined(QT_QPA_PLATFORM_XCB)
+Q_IMPORT_PLUGIN(QXcbIntegrationPlugin);
+#elif defined(QT_QPA_PLATFORM_WINDOWS)
 Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
 #elif defined(QT_QPA_PLATFORM_COCOA)
 Q_IMPORT_PLUGIN(QCocoaIntegrationPlugin);
